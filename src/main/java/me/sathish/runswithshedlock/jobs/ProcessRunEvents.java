741c3f86a3ec3ee71c0f3ec5b8fc2aeebf562985
Sathish Jayapal <sjayapal@skminfotech.com>
2025-06-01 18:37:45 -0500

    Event ready service changes

---
+++ src/main/java/me/sathish/runswithshedlock/jobs/ProcessRunEvents.java
@@ -0,0 +1,13 @@
+package me.sathish.runswithshedlock.jobs;
+
+import net.javacrumbs.shedlock.core.LockAssert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+public class ProcessRunEvents {
+}
+

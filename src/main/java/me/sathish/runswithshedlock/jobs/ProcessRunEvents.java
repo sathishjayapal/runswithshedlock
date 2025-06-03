d6aabc3e451eb97ac1b82cc54976ba31d7d9b5d0
Sathish Jayapal <sjayapal@skminfotech.com>
2025-06-02 21:38:35 -0500

    Process Data

--- src/main/java/me/sathish/runswithshedlock/jobs/ProcessRunEvents.java
+++ src/main/java/me/sathish/runswithshedlock/jobs/ProcessRunEvents.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ProcessRunEvents {
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 100000)
     public void processRunEvents() {
         System.out.println("Processing run events...");
     }
